648ad7ccc050a4fca7a3d7ca8ce61554e4718f66
elias_dahi@hotmail.com <elias_dahi@hotmail.com>
2021-06-16 07:18:19 +0300

    Adding NgxPrintModule to the root App

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
+import { NgxPrintModule } from 'ngx-print';
 
 import {
   AppRoutingModule,
@@ -18,6 +19,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    NgxPrintModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
